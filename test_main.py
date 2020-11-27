c0ed3806b36bbb0eedcf919c8dcba56d4fe390fd
SeuRonão <22750712+SeuRonao@users.noreply.github.com>
2020-11-26 21:23:47 -0300

    Remove bug dos testes

--- test_main.py
+++ test_main.py
@@ -13,7 +13,6 @@ class Test(unittest.TestCase):
         """Função que testa se a saída do programa corresponde ao que foi especificado."""
         # Lista de valores que serão retornados pela função input.
         input_returns = [str(random.randint(1, 200))]
-        print('Testando com', input_returns[0])
         with patch('builtins.input',
                    side_effect=input_returns) as mock_input, patch(
                        'builtins.print') as mock_print:
@@ -21,7 +20,7 @@ class Test(unittest.TestCase):
             mock_input.assert_called()
             result = pi * float(input_returns[0])**2
             result = f'A área do círculo é {result:.2f}'
-            mock_print.called_with(result)
+            mock_print.assert_called_with(result)
 
 
 if __name__ == '__main__':
